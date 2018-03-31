e3577982680cac25f89d5362922e180d414618de
djorna <davidjorna@gmail.com>
2018-03-31 16:43:54 -0400

    Scaled force input

--- Controls/select_data.m
+++ Controls/select_data.m
@@ -2,11 +2,13 @@ load('S2_E3_A1.mat');
 emg44 = emg(stimulus==44,:);
 data_sample = emg44(:,1);
 
-% plot
-plot(data_sample);
-% fft
-figure
-plot(abs(fft(data_sample)));
+emg50 = emg(stimulus==0,1);
+
+% % plot
+% plot(data_sample);
+% % fft
+% figure
+% plot(abs(fft(data_sample)));
 
 % get time vector (for use with simulink)
 m = size(data_sample,1);
@@ -18,3 +20,10 @@ t = linspace(0, m * T, m);
 emg_input = double([t.' data_sample]);
 f44 = force(stimulus==44,4);
 force_data = double([t.' f44]);
+
+
+m = size(emg50,1);
+T = 1 / 2e3;
+t = linspace(0, m * T, m);
+
+emg_rest = double([t.' emg50]);
